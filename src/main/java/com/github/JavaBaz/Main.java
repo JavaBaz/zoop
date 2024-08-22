38e0037629cf9ca6cd206bb0c745d173dab030b9
MohammadAmin HosseinNiya <m.amin.hosseinniya@gmail.com>
2024-08-22 19:54:20 +0330

    feat: Add an attack event from shirshah upon bufal

--- src/main/java/com/github/JavaBaz/Main.java
+++ src/main/java/com/github/JavaBaz/Main.java
@@ -1,6 +1,8 @@
 package com.github.JavaBaz;
 
+import com.github.JavaBaz.animals.mammals.felines.AbstractFelines;
 import com.github.JavaBaz.animals.mammals.felines.Lion;
+import com.github.JavaBaz.animals.mammals.primates.Orangutan;
 import com.github.JavaBaz.infra.FoodType;
 import com.github.JavaBaz.infra.Origin;
 
@@ -18,5 +20,25 @@ public class Main {
                 FoodType.CARNIVORES);
 
         shirshah.sleep();
+
+
+        Orangutan boufal = new Orangutan(false,
+                "boufal",
+                LocalDate.now().minusMonths(4),
+                Origin.ASIA,
+                FoodType.OMNIVORES);
+
+        System.out.println("boufal has an attack power of " + boufal.getAttackPower() +
+                            " and a defence power of " + boufal.getDefencePower());
+        System.out.println("shirshah has an attack power of " + shirshah.getAttackPower() +
+                            " a defence power of " + shirshah.getDefencePower());
+        shirshah.attack(boufal);
+
+        System.out.println("\nThis is what's happened after the attack: ");
+
+        System.out.println("boufal has an attack power of " + boufal.getAttackPower() +
+                " and a defence power of " + boufal.getDefencePower());
+        System.out.println("shirshah has an attack power of " + shirshah.getAttackPower() +
+                " a defence power of " + shirshah.getDefencePower());
     }
 }
